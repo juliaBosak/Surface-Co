13ad013f0c9ed38cb9306121440ae4e17c2c0126
juliabosak <1kn15b.bosak@gmail.com>
2019-09-15 16:03:52 +0300

    Revert "Merge branch 'develop' into patch/content/about-us_section"
    This reverts commit aacac569ec693632f11e60a0f4140198bfd14f93, reversing
    changes made to a01ed76675b844a6229bb32530a74c3ee75aa46e.

--- src/js/style.js
+++ src/js/style.js
@@ -30,6 +30,7 @@ boardPrev.addEventListener('click', () => {
 		}
 	}
 }) ();
+
 (function() {
 	const 
 		videoButton = document.getElementById('about-us_video-button'),
@@ -42,3 +43,4 @@ boardPrev.addEventListener('click', () => {
 
 		
 }) ();
+
